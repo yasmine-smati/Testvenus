76dee2fdef9ca2bd00dbf0dfe042bfdda3c9ca53
yasmine-smati <smatiyasmine10@gmail.com>
2024-09-05 09:19:43 +0100

    traitement_ajout_outil.php

--- index.php
+++ index.php
@@ -1,40 +1,30 @@
 <?php
-// Informations de connexion à la base de données
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "test";
+// traitement_ajout_outil.php
 
-// Créer une connexion
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Vérifier la connexion
-if ($conn->connect_error) {
-    die("Échec de la connexion : " . $conn->connect_error);
-}
+include 'db_connection.php'; // Inclure le fichier de connexion
 
 // Vérifier si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les données du formulaire
-    $type = $_POST['type'];
-    $marque = $_POST['marque'];
-    $modele = $_POST['modele'];
-    $num_serie = $_POST['num_serie'];
-    $date_achat = $_POST['date_achat'];
-    $etat = $_POST['etat'];
-    $localisation = $_POST['localisation'];
+    $outilType = $_POST['type'];
+    $outilMarque = $_POST['marque'];
+    $outilModele = $_POST['modele'];
+    $outilNumSerie = $_POST['num_serie'];
+    $outilDateAchat = $_POST['date_achat'];
+    $outilEtat = $_POST['etat'];
+    $outilLocalisation = $_POST['localisation'];
 
     // Préparer la requête SQL
-    $sql = "INSERT INTO outils (type, marque, modele, num_serie, date_achat, etat, localisation) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    $sqlInsertOutil = "INSERT INTO outils (type, marque, modele, num_serie, date_achat, etat, localisation) VALUES (?, ?, ?, ?, ?, ?, ?)";
     
     // Préparer et lier
-    if ($stmt = $conn->prepare($sql)) {
-        $stmt->bind_param("sssssss", $type, $marque, $modele, $num_serie, $date_achat, $etat, $localisation);
+    if ($stmt = $conn->prepare($sqlInsertOutil)) {
+        $stmt->bind_param("sssssss", $outilType, $outilMarque, $outilModele, $outilNumSerie, $outilDateAchat, $outilEtat, $outilLocalisation);
         
         // Exécuter la requête
         if ($stmt->execute()) {
-            // Redirection vers la liste des outils après l'ajout
-            header("Location: index2.php");
+            // Redirection vers la page de confirmation après l'ajout
+            header("Location: confirmation.php?type=$outilType&marque=$outilMarque&modele=$outilModele&num_serie=$outilNumSerie&date_achat=$outilDateAchat&etat=$outilEtat&localisation=$outilLocalisation");
             exit();
         } else {
             echo "<div class='alert alert-danger mt-3'>Erreur lors de l'exécution de la requête : " . $stmt->error . "</div>";
